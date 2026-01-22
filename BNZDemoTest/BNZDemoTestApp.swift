c5c67390ff0cee75261ae0b02c9c1c1e76fe9ddd
jeuneseven <jeuneseven@gmail.com>
2026-01-22 13:42:33 +1300

    Initial Commit

---
+++ BNZDemoTest/BNZDemoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BNZDemoTestApp.swift
+//  BNZDemoTest
+//
+//  Created by seven on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BNZDemoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BNZDemoTestTests/BNZDemoTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BNZDemoTestTests.swift
+//  BNZDemoTestTests
+//
+//  Created by seven on 2026/1/22.
+//
+
+import Testing
+@testable import BNZDemoTest
+
+struct BNZDemoTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
